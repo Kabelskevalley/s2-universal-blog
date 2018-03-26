4a9b2d0533d65eedbaa08ef4a461deb832e8058d
Fabian Hicksch <fabian.hicksch@icloud.com>
2018-03-26 14:08:40 +0200

    Lauffähiges Laravel in netbeans

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hallo
                 </div>
 
                 <div class="links">
